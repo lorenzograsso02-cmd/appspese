2db75b7822fab42762e83ea288ca7f1c8156f6fd
lorenzograsso02-cmd <lorenzo.grasso.02@gmail.com>
2026-08-23 12:38:55 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'le-mie-spese-v6';
+const CACHE_NAME = 'le-mie-spese-v7';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
